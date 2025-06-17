6cefe1d8d04a8b8d336db5901a00b72ae4ffbc70
Renan <renansaueia@gmail.com>
2025-06-17 17:08:56 -0300

    a few more

--- cpp-01/ex05/main.cpp
+++ cpp-01/ex05/main.cpp
@@ -5,21 +5,21 @@ int main(int c, char **v)
 	if (c != 2)
 	{
 		std::cout << "Usage: ./harl <complaint>" << std::endl;
-		std::cout << "complaints are: DEBUG, INFO, WARNING or ERROR" << std::endl;
+		std::cout << "complaints are: DEBUG, INFO, WARNING or ERROR - in all CAPS!" << std::endl;
 		return 1;
 	}
 	std::string level = v[1];
 	Harl harl;
-	if (level == "DEBUG" || level == "debug")
+	if (level == "DEBUG")
 		harl.complain("DEBUG");
-	else if (level == "INFO" || level == "info")
+	else if (level == "INFO")
 		harl.complain("INFO");
-	else if (level == "WARNING" || level == "warning")
+	else if (level == "WARNING")
 		harl.complain("WARNING");
-	else if (level == "ERROR" || level == "error")
+	else if (level == "ERROR")
 		harl.complain("ERROR");
 	else
-		std::cout << "[ Probably complaining about insignificant problems - Harl is such a Karen lol!]" << std::endl;
+		std::cout << "[ Probably complaining about insignificant problems - Harl is such a Karen lol! ]" << std::endl;
 
 	//harl.complain("DEBUG");
 	//harl.complain("INFO");

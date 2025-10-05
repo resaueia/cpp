40a946fc81713fe7959e8205aac95b87f6723cc1
Renan <renansaueia@gmail.com>
2025-10-05 18:06:59 -0300

    uploading the start of the next list, just some pre-studying

---
+++ 06-cpp/practice/00.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   00.cpp                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsaueia <rsaueia@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 17:33:38 by rsaueia           #+#    #+#             */
+/*   Updated: 2025/10/05 17:34:26 by rsaueia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+
+| Tipo de cast       | Uso principal                                                                | Equivalente (aproximado) em C   |
+| ------------------ | ---------------------------------------------------------------------------- | ------------------------------- |
+| `static_cast`      | conversões entre tipos compatíveis em tempo de compilação                    | `(type)value`                   |
+| `reinterpret_cast` | conversões perigosas entre tipos *não relacionados* (ex: ponteiro → inteiro) | `(type)value`                   |
+| `const_cast`       | adiciona ou remove o modificador `const`                                     | `(type)value`                   |
+| `dynamic_cast`     | conversões seguras entre tipos relacionados por herança                      | *(não existe equivalente em C)* |
+
+*/
+
